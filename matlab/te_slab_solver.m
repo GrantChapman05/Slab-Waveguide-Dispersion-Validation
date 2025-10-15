e1dd6dc7c35474ce7a0b798b24f5441fce19ad18
Grant Chapman <grant.chapman05@gmail.com>
2025-10-15 01:57:38 -0400

    Add files via upload

---
+++ matlab/te_slab_solver.m
@@ -0,0 +1,24 @@
+%Values to be passed in
+t_nm
+lambda_nm
+n_core
+n_clad
+mode
+
+%values to be output
+n_eff
+status
+
+%Convert units to m instead of nm
+
+%Compute helper values and find whether TE1 and TE0 even exist
+
+%Use V for existence check of TE1 and TE0
+
+%Determine whether even or odd mode is best to solve
+
+%MATLAB root finder for the u that makes the eqn true
+
+%Bring all together for neff
+
+%polish and output
